9d23de4f8ac79c77584e30aefaa46faa6ec8cc87
webGrandpa <goga.khilo@gmail.com>
2025-04-06 21:07:25 +0400

    exercises from gemini is passed

--- JSpractice/gemini.js
+++ JSpractice/gemini.js
@@ -35,155 +35,450 @@
 
 //easy exercises
 
-//1. area calculator:
-let width = 10;
-let height = 20;
-let area = width * height;
+// //1. area calculator:
+// let width = 10;
+// let height = 20;
+// let area = width * height;
 
-console.log(area);
+// console.log(area);
 
-//2. reminder check:
+// //2. reminder check:
+
+// let num = 15;
+// let reminder = num % 4;
+
+// console.log(reminder);
+
+// //3. incrementing counter:
+
+// let counter = 0;
+// counter ++;
+// counter ++;
+// counter ++;
+
+// console.log(counter);
+
+// //4. String greeting:
+
+// let greeting = 'hello';
+// let name = 'learner';
+// let fullgreeting = (greeting + ' ' + name);
+
+// console.log(fullgreeting);
+
+// //5. simple comparison:
+
+// let myAge = 31;
+
+// console.log(myAge === 18);
+
+// //Medium exercises
+
+// //1. Temperature Converter(c to f):
+
+// let tempCelsius = 21;
+// let tempFahrenheit = (tempCelsius * 9/5) + 32;
+
+// console.log(`${tempCelsius}°C is ${tempFahrenheit}°F`);
+
+// //2. Discount Calculator:
+
+// let originalPrice = 100;
+// let discountPercentage = 20;
+
+// let finalPrice = originalPrice - (originalPrice * (discountPercentage / 100));
+
+// console.log(finalPrice);
+
+// //3. Login Check:
+
+// let isUserLoggedIn = true;
+// let hasPurchaseCredit = false;
+
+// let canMakePurchase = isUserLoggedIn && hasPurchaseCredit;
+
+// console.log(canMakePurchase);
+
+// //4. Access Check:
+
+// let isAdmin = true;
+// let isEditor = false;
+
+// let hasAccessToEdit = isAdmin || isEditor;
+
+// console.log(hasAccessToEdit);
+
+// //5. Eligibity Check:
+
+// let userScore = 75;
+// let result = (userScore > 50) ? 'pass' : 'fail';
+
+// console.log(result);
+
+// //Hard Exercises
+
+// //1. Complex Calculation & Type Check:
+
+// let a = 5;
+// let b = '10';
+// let c = 3;
+// let result1 = a + c * 2.
+// console.log(result1);
+// let result2 = a + b + c;
+// console.log(result2);
+// let result3 = b + a + c;
+// console.log(result3);
+
+// console.log(typeof(result1),
+//             typeof(result2),
+//             typeof(result3));
+
+
+// //2. Strict vs Loose Comparison Investigation:
+
+// let val1 = 0;
+// let val2 = false;
+// let val3 = '0';
+// let val4 = null;
+// let val5 = undefined;
+// console.log(val1 == val2);
+// console.log(val1 === val2);
+// console.log(val1 == val3);
+// console.log(val1 === val3);
+// console.log(val2 == val3);
+// console.log(val4 == val5);
+// console.log(val4 === val5);
+// //== and === gives different results because of theyr properties
+// //== checking just a value of this pair
+// //=== checking also a type of pair
+
+// //3. Logical Combination & Short-Circuiting:
+
+// let isAvailable = true;
+// let quantity = 0;
+// let canOrder = false;
+// canOrder = isAvailable || quantity > 0;
+// console.log(canOrder);
+// canOrder = isAvailable && quantity > 0;
+// console.log(canOrder);
+// canOrder != isAvailable || quantity > 0;
+// console.log(canOrder);
+
+// canOrder = true;
+// canOrder = isAvailable || quantity !== 0;
+// console.log(canOrder);
+
+// //4. Pre/Post Increment/Decrement Puzzle:
+
+// let x = 5;
+// let y = 10;
+
+// console.log(x++ + y);
+// console.log(++x + --y);
+// console.log(x + y--);
+// console.log(x + y);
+
+// //5. Ternary Chaining (Simplified):
+
+// let score = 75;
+// let grade = (score >= 90) ? 'A'
+//            :(score >= 70) ? 'B'
+//            :(score >= 50) ? 'C'
+//            :'D';
+
+// console.log(grade);
+
+//Quiz Time! (Days 1-4)
+
+// 1 - c
+// 2 - a
+// 3 - d
+// 4 - c
+// 5 - b
+// 6 - d
+// 7 - c
+// 8 - b
+// 9 - c
+//10 - c
+//11 - true
+//12 - true
+//13 - false
+//14 - false
+//15 - false
+//16 - true
+//17 - false
+//18 - false
+//19 - false
+//20 - false
+//21 - NaN, '0', -0, 0n, 0, ''. 
+//22 - switch
+//23 - true
+//24 - condition : '';
+//25 - "Greater"
+//26 - "Falsy"
+//27 - truthy
+//28 - 'Alpha','Bravo','Charlie';
+//29 - false
+//30 - true
+//31 - "Adult"
+//32 - console
+//33 - NaN
+
+//Easy Exercises:
+
+//1.Check Number Sign:
 
 let num = 15;
-let reminder = num % 4;
 
-console.log(reminder);
+if (num > 0){
+    console.log('Positive');
+} else if (num < 0){
+    console.log('Negative');
+} else {
+    console.log('0 is not a negative or positive');
+}
 
-//3. incrementing counter:
+//2.Check if Even:
 
-let counter = 0;
-counter ++;
-counter ++;
-counter ++;
+let value = 9;
 
-console.log(counter);
+if(value % 2 === 0){
+    console.log('even');
+} else if (value % 2 > 0){
+    console.log('odd');
+} else {
+    console.log('0 is not a odd or even');
+}
 
-//4. String greeting:
+//3.Simple Greeting:
 
-let greeting = 'hello';
-let name = 'learner';
-let fullgreeting = (greeting + ' ' + name);
+let hour = 18;
 
-console.log(fullgreeting);
+if(hour > 12 && hour < 23){
+    console.log('Good morning');
+} else {
+    console.log('Good evening');
+}
 
-//5. simple comparison:
+//4.Basic Switch:
 
-let myAge = 31;
+let color = 'green';
 
-console.log(myAge === 18);
+switch (color){
+    case 'red':
+        console.log('Stop');
+        break;
+    case 'green':
+        console.log('Go');
+        break;
+    case 'blue':
+        console.log('Ready');
+        break;
+    default:
+        console.log('Unknown color');
+}
 
-//Medium exercises
+//5.Truthy/Falsy Check:
 
-//1. Temperature Converter(c to f):
+let userInput = null;
 
-let tempCelsius = 21;
-let tempFahrenheit = (tempCelsius * 9/5) + 32;
+if(userInput !== "" && userInput !== null && userInput !== 'hello' && userInput !== 0 && userInput !== NaN){
+    console.log('Input received');
+} else {
+    console.log('No input provided');
+}
 
-console.log(`${tempCelsius}°C is ${tempFahrenheit}°F`);
+//Medium Exercises
 
-//2. Discount Calculator:
+//1.Detailed Greeting:
 
-let originalPrice = 100;
-let discountPercentage = 20;
+if(hour < 12){
+    console.log('Good morning!');
+} else if (hour >= 12 && hour <= 18){
+    console.log('Good afternoon');
+} else {
+    console.log('Good evening');
+}
 
-let finalPrice = originalPrice - (originalPrice * (discountPercentage / 100));
+//2.Number Ranges:
 
-console.log(finalPrice);
+let number = 8;
 
-//3. Login Check:
+if(number < 10){
+    console.log('Small');
+} else if (number >= 10 && number < 100){
+    console.log('Medium');
+} else {
+    console.log('Large');
+}
 
-let isUserLoggedIn = true;
-let hasPurchaseCredit = false;
+//3.Login Logic:
 
-let canMakePurchase = isUserLoggedIn && hasPurchaseCredit;
+let username = 'webGrandpa';
+let password = 'barbareIliasaMari';
 
-console.log(canMakePurchase);
+if(username !== '' && password.length > 6){
+    console.log('Login successful');
+} else {
+    console.log('Login failed');
+}
 
-//4. Access Check:
+//4.Ticket Pricing:
 
-let isAdmin = true;
-let isEditor = false;
+let age = 19;
 
-let hasAccessToEdit = isAdmin || isEditor;
+if(age < 5){
+    console.log('Free');
+} else if (age >= 5 && age <= 12){
+    console.log("Child Ticket: $5");
+} else if (age >= 13 && age <= 59){
+    console.log("Adult Ticket: $10");
+} else {
+    console.log("Senior Ticket: $7");
+}
 
-console.log(hasAccessToEdit);
+//5.Switch with Fall-through (Days of the Week):
 
-//5. Eligibity Check:
+let dayNumber = '4';
 
-let userScore = 75;
-let result = (userScore > 50) ? 'pass' : 'fail';
-
-console.log(result);
+switch(dayNumber){
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+        console.log('Weekday');
+        break;
+    case '6':
+    case '7':
+        console.log('Weekend');
+        break;
+    default:'Invalid numbers'
+}
 
 //Hard Exercises
 
-//1. Complex Calculation & Type Check:
+//1.Nested Access Control:
 
-let a = 5;
-let b = '10';
-let c = 3;
-let result1 = a + c * 2.
-console.log(result1);
-let result2 = a + b + c;
-console.log(result2);
-let result3 = b + a + c;
-console.log(result3);
+let isLoggedIn = true;
+let userRole = 'admin';
+let documentStatus = 'draft';
 
-console.log(typeof(result1),
-            typeof(result2),
-            typeof(result3));
+if(!isLoggedIn){
+    console.log('Please log in.');
+} else {
+    if(userRole === 'admin'){
+        console.log('Admin can manage all documents.');
+    } else if (userRole === 'editor'){
+        if(documentStatus === 'draft'){
+            console.log('Editor can edit drafts.');
+        } else if (documentStatus === 'published'){
+            console.log('Editor cannot edit published documents.');
+        } else {
+            console.log('Invalid status');
+        }
+    } else if(userRole === 'guest'){
+        if(documentStatus === 'published'){
+            console.log('Guest can view published documents.')
+        } else if (documentStatus === 'draft'){
+            console.log('Guest cannot view drafts.')
+        } else {
+            console.log('Invalid status');
+        } 
+    } else {
+        console.log('Uncnown role.')
+    }
+}
 
+//2.Simple Discount Calculator:
 
-//2. Strict vs Loose Comparison Investigation:
+let purchaseAmount = 10;
+let isMember = true;
 
-let val1 = 0;
-let val2 = false;
-let val3 = '0';
-let val4 = null;
-let val5 = undefined;
-console.log(val1 == val2);
-console.log(val1 === val2);
-console.log(val1 == val3);
-console.log(val1 === val3);
-console.log(val2 == val3);
-console.log(val4 == val5);
-console.log(val4 === val5);
-//== and === gives different results because of theyr properties
-//== checking just a value of this pair
-//=== checking also a type of pair
+if(purchaseAmount >= 100){
+    if(isMember === true){
+        console.log(`Amount: ${purchaseAmount}, discount 15%, Final price: ${purchaseAmount - (purchaseAmount * 15 / 100)}`);
+    } else {
+        console.log(`Amount: ${purchaseAmount}, discount 10%, Final price: ${purchaseAmount - (purchaseAmount * 10 / 100)}`);
+    }
+} else if(purchaseAmount < 100){
+    if(isMember === true){
+        console.log(`Amount: ${purchaseAmount}, discount 5%, Final price: ${purchaseAmount - (purchaseAmount * 5 / 100)}`);
+    } else {
+        console.log(`Amount: ${purchaseAmount}, discount 0%, Final price: ${purchaseAmount}`);
+    }
+}else {
+    console.log(purchaseAmount);
+}
 
-//3. Logical Combination & Short-Circuiting:
+//3.Rock Paper Scissors Logic:
 
-let isAvailable = true;
-let quantity = 0;
-let canOrder = false;
-canOrder = isAvailable || quantity > 0;
-console.log(canOrder);
-canOrder = isAvailable && quantity > 0;
-console.log(canOrder);
-canOrder != isAvailable || quantity > 0;
-console.log(canOrder);
+let player1Choice = 'rock';
+let player2Choice = 'paper';
 
-canOrder = true;
-canOrder = isAvailable || quantity !== 0;
-console.log(canOrder);
+if (player1Choice === 'rock'){
+    if (player2Choice === 'paper'){
+        console.log('Player 2 wins!');
+    } else if (player2Choice === 'scissors'){
+        console.log('Player 1 wins!');
+    } else {
+        console.log("it's a tie!");
+    }
+} else if (player1Choice === 'paper'){
+    if (player2Choice === 'paper'){
+        console.log("it's a tie!");
+    } else if (player2Choice === 'scissors'){
+        console.log('Player 2 wins!');
+    } else {
+        console.log('Player 1 wins!');
+    }
+} else if (player1Choice === 'scissors'){
+    if (player2Choice === 'paper'){
+        console.log('Player 1 wins!');
+    } else if (player2Choice === 'scissors'){
+        console.log("it's a tie!");
+    } else {
+        console.log('Player 2 wins!');
+    }
+} else {
+    console.log('invalid move');
+}
 
-//4. Pre/Post Increment/Decrement Puzzle:
+//4.Grade Calculator using Switch (Truthy expression):
 
-let x = 5;
-let y = 10;
+let score = 90;
 
-console.log(x++ + y);
-console.log(++x + --y);
-console.log(x + y--);
-console.log(x + y);
+switch(true){
+    case score >= 90:
+        console.log("Grade: A");
+        break;
+    case score >= 80:
+        console.log("Grade: B");
+        break;
+    case score >= 70:
+        console.log("Grade: C");
+        break;
+    case score >= 60:
+        console.log("Grade: D");
+        break;
+    default: 
+        console.log("grade: F")
+}
 
-//5. Ternary Chaining (Simplified):
+//5.Character Class Assignment:
 
-let score = 75;
-let grade = (score >= 90) ? 'A'
-           :(score >= 70) ? 'B'
-           :(score >= 50) ? 'C'
-           :'D';
+let strength = 40;
+let intelligence = 45;
+let className = '';
 
-console.log(grade);
+if(strength > 15 && intelligence <= 10){
+    className = 'Warrior';
+} else if (strength <= 10 && intelligence > 15){
+    className = 'Mage';
+} else if (strength > 10 && intelligence > 10){
+    className = 'Spellsword';
+} else {
+    className = 'Advanturer'
+}
+
+console.log(className);
