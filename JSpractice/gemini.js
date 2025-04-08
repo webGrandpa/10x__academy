0b30c51b892720a79900280bc91389a77e393447
webGrandpa <goga.khilo@gmail.com>
2025-04-08 23:56:22 +0400

    Js practice day

--- JSpractice/gemini.js
+++ JSpractice/gemini.js
@@ -228,257 +228,453 @@
 
 //1.Check Number Sign:
 
-let num = 15;
+// let num = 15;
 
-if (num > 0){
-    console.log('Positive');
-} else if (num < 0){
-    console.log('Negative');
-} else {
-    console.log('0 is not a negative or positive');
+// if (num > 0){
+//     console.log('Positive');
+// } else if (num < 0){
+//     console.log('Negative');
+// } else {
+//     console.log('0 is not a negative or positive');
+// }
+
+// //2.Check if Even:
+
+// let value = 9;
+
+// if(value % 2 === 0){
+//     console.log('even');
+// } else if (value % 2 > 0){
+//     console.log('odd');
+// } else {
+//     console.log('0 is not a odd or even');
+// }
+
+// //3.Simple Greeting:
+
+// let hour = 18;
+
+// if(hour > 12 && hour < 23){
+//     console.log('Good morning');
+// } else {
+//     console.log('Good evening');
+// }
+
+// //4.Basic Switch:
+
+// let color = 'green';
+
+// switch (color){
+//     case 'red':
+//         console.log('Stop');
+//         break;
+//     case 'green':
+//         console.log('Go');
+//         break;
+//     case 'blue':
+//         console.log('Ready');
+//         break;
+//     default:
+//         console.log('Unknown color');
+// }
+
+// //5.Truthy/Falsy Check:
+
+// let userInput = null;
+
+// if(userInput !== "" && userInput !== null && userInput !== 'hello' && userInput !== 0 && userInput !== NaN){
+//     console.log('Input received');
+// } else {
+//     console.log('No input provided');
+// }
+
+// //Medium Exercises
+
+// //1.Detailed Greeting:
+
+// if(hour < 12){
+//     console.log('Good morning!');
+// } else if (hour >= 12 && hour <= 18){
+//     console.log('Good afternoon');
+// } else {
+//     console.log('Good evening');
+// }
+
+// //2.Number Ranges:
+
+// let number = 8;
+
+// if(number < 10){
+//     console.log('Small');
+// } else if (number >= 10 && number < 100){
+//     console.log('Medium');
+// } else {
+//     console.log('Large');
+// }
+
+// //3.Login Logic:
+
+// let username = 'webGrandpa';
+// let password = 'barbareIliasaMari';
+
+// if(username !== '' && password.length > 6){
+//     console.log('Login successful');
+// } else {
+//     console.log('Login failed');
+// }
+
+// //4.Ticket Pricing:
+
+// let age = 19;
+
+// if(age < 5){
+//     console.log('Free');
+// } else if (age >= 5 && age <= 12){
+//     console.log("Child Ticket: $5");
+// } else if (age >= 13 && age <= 59){
+//     console.log("Adult Ticket: $10");
+// } else {
+//     console.log("Senior Ticket: $7");
+// }
+
+// //5.Switch with Fall-through (Days of the Week):
+
+// let dayNumber = '4';
+
+// switch(dayNumber){
+//     case '1':
+//     case '2':
+//     case '3':
+//     case '4':
+//     case '5':
+//         console.log('Weekday');
+//         break;
+//     case '6':
+//     case '7':
+//         console.log('Weekend');
+//         break;
+//     default:'Invalid numbers'
+// }
+
+// //Hard Exercises
+
+// //1.Nested Access Control:
+
+// let isLoggedIn = true;
+// let userRole = 'admin';
+// let documentStatus = 'draft';
+
+// if(!isLoggedIn){
+//     console.log('Please log in.');
+// } else {
+//     if(userRole === 'admin'){
+//         console.log('Admin can manage all documents.');
+//     } else if (userRole === 'editor'){
+//         if(documentStatus === 'draft'){
+//             console.log('Editor can edit drafts.');
+//         } else if (documentStatus === 'published'){
+//             console.log('Editor cannot edit published documents.');
+//         } else {
+//             console.log('Invalid status');
+//         }
+//     } else if(userRole === 'guest'){
+//         if(documentStatus === 'published'){
+//             console.log('Guest can view published documents.')
+//         } else if (documentStatus === 'draft'){
+//             console.log('Guest cannot view drafts.')
+//         } else {
+//             console.log('Invalid status');
+//         } 
+//     } else {
+//         console.log('Uncnown role.')
+//     }
+// }
+
+// //2.Simple Discount Calculator:
+
+// let purchaseAmount = 10;
+// let isMember = true;
+
+// if(purchaseAmount >= 100){
+//     if(isMember === true){
+//         console.log(`Amount: ${purchaseAmount}, discount 15%, Final price: ${purchaseAmount - (purchaseAmount * 15 / 100)}`);
+//     } else {
+//         console.log(`Amount: ${purchaseAmount}, discount 10%, Final price: ${purchaseAmount - (purchaseAmount * 10 / 100)}`);
+//     }
+// } else if(purchaseAmount < 100){
+//     if(isMember === true){
+//         console.log(`Amount: ${purchaseAmount}, discount 5%, Final price: ${purchaseAmount - (purchaseAmount * 5 / 100)}`);
+//     } else {
+//         console.log(`Amount: ${purchaseAmount}, discount 0%, Final price: ${purchaseAmount}`);
+//     }
+// }else {
+//     console.log(purchaseAmount);
+// }
+
+// //3.Rock Paper Scissors Logic:
+
+// let player1Choice = 'rock';
+// let player2Choice = 'paper';
+
+// if (player1Choice === 'rock'){
+//     if (player2Choice === 'paper'){
+//         console.log('Player 2 wins!');
+//     } else if (player2Choice === 'scissors'){
+//         console.log('Player 1 wins!');
+//     } else {
+//         console.log("it's a tie!");
+//     }
+// } else if (player1Choice === 'paper'){
+//     if (player2Choice === 'paper'){
+//         console.log("it's a tie!");
+//     } else if (player2Choice === 'scissors'){
+//         console.log('Player 2 wins!');
+//     } else {
+//         console.log('Player 1 wins!');
+//     }
+// } else if (player1Choice === 'scissors'){
+//     if (player2Choice === 'paper'){
+//         console.log('Player 1 wins!');
+//     } else if (player2Choice === 'scissors'){
+//         console.log("it's a tie!");
+//     } else {
+//         console.log('Player 2 wins!');
+//     }
+// } else {
+//     console.log('invalid move');
+// }
+
+// //4.Grade Calculator using Switch (Truthy expression):
+
+// let score = 90;
+
+// switch(true){
+//     case score >= 90:
+//         console.log("Grade: A");
+//         break;
+//     case score >= 80:
+//         console.log("Grade: B");
+//         break;
+//     case score >= 70:
+//         console.log("Grade: C");
+//         break;
+//     case score >= 60:
+//         console.log("Grade: D");
+//         break;
+//     default: 
+//         console.log("grade: F")
+// }
+
+// //5.Character Class Assignment:
+
+// let strength = 40;
+// let intelligence = 45;
+// let className = '';
+
+// if(strength > 15 && intelligence <= 10){
+//     className = 'Warrior';
+// } else if (strength <= 10 && intelligence > 15){
+//     className = 'Mage';
+// } else if (strength > 10 && intelligence > 10){
+//     className = 'Spellsword';
+// } else {
+//     className = 'Advanturer'
+// }
+
+// console.log(className);
+
+//Quiz Time! (Days 1-6)
+
+// 1 - true
+// 2 - true
+// 3 - false
+// 4 - true
+// 5 - false
+// 6 - false
+// 7 - false
+// 8 - false
+// 9 - false
+//10 - true
+//11 - true
+//12 - true
+//13 - c
+//14 - d
+//15 - d
+//16 - c
+//17 - c
+//18 - b
+//19 - c
+//20 - c
+//21 - parametres
+//22 - return
+//23 - expresion
+//24 - scope
+//25 - global
+//26 - arguments
+//27 - false
+//28 - Inside, undefined
+//29 - multiply(6, 7)
+//30 - (true This won't run) and (false This won't run)
+//31 - 2
+//32 - 8 undefined
+//33 - error
+
+//Easy Exercises
+
+//1. Simple Greeter:
+
+function greet(name){
+    console.log(`Hello ${name}`);
+}
+greet('Gio');
+greet('Zurbajana')
+
+//2. Sum Function:
+
+function calculateSum(num1, num2){
+    return num1 + num2;
 }
 
-//2.Check if Even:
+let sumNums = calculateSum(3, 4);
+console.log(sumNums);
 
-let value = 9;
+//3. Basic Arrow Function:
 
-if(value % 2 === 0){
-    console.log('even');
-} else if (value % 2 > 0){
-    console.log('odd');
-} else {
-    console.log('0 is not a odd or even');
+let isPositive = (num) => {
+    if(num > 0){
+        console.log(ture);
+    } else {
+        console.log(false);
+    }
 }
 
-//3.Simple Greeting:
+//4. Function Call Practice:
 
-let hour = 18;
-
-if(hour > 12 && hour < 23){
-    console.log('Good morning');
-} else {
-    console.log('Good evening');
+function displayInfo(item, price){
+    return`${item} costs ${price}`;
 }
 
-//4.Basic Switch:
+console.log(displayInfo("coffee", 3));
+console.log(displayInfo("book", 15));
+console.log(displayInfo("keyboard", 75));
 
-let color = 'green';
+//5. Return Undefined:
 
-switch (color){
-    case 'red':
-        console.log('Stop');
-        break;
-    case 'green':
-        console.log('Go');
-        break;
-    case 'blue':
-        console.log('Ready');
-        break;
-    default:
-        console.log('Unknown color');
+function logOnly(message){
+    console.log(message);
 }
 
-//5.Truthy/Falsy Check:
-
-let userInput = null;
-
-if(userInput !== "" && userInput !== null && userInput !== 'hello' && userInput !== 0 && userInput !== NaN){
-    console.log('Input received');
-} else {
-    console.log('No input provided');
-}
-
+logOnly('some message');
+let result = logOnly();
+console.log(logOnly());
 //Medium Exercises
 
-//1.Detailed Greeting:
+//6.Check Age:
 
-if(hour < 12){
-    console.log('Good morning!');
-} else if (hour >= 12 && hour <= 18){
-    console.log('Good afternoon');
-} else {
-    console.log('Good evening');
+function canVote(age){
+    if(age > 18){
+        return "Eligible to vote";
+    } else {
+        return "Not eligible to vote";
+    }
+}
+canVote();
+console.log(canVote(25));
+console.log(canVote(16));
+
+//7. Print Numbers with Loop:
+
+function printNumbersUpTo(limit){
+    for(let i = 1; i <= limit; i++){
+        console.log(i);
+    }
 }
 
-//2.Number Ranges:
+printNumbersUpTo(6);
 
-let number = 8;
+//8.Count Down:
 
-if(number < 10){
-    console.log('Small');
-} else if (number >= 10 && number < 100){
-    console.log('Medium');
-} else {
-    console.log('Large');
+let countDownFrom = function(start){
+    while(start > 0){
+        start--
+        console.log(start);
+    }
+    console.log("Blast Off!");
 }
 
-//3.Login Logic:
+countDownFrom(5);
 
-let username = 'webGrandpa';
-let password = 'barbareIliasaMari';
+//9. String Repeater:
 
-if(username !== '' && password.length > 6){
-    console.log('Login successful');
-} else {
-    console.log('Login failed');
+function repeatString(text, count){
+    let resultString = '';
+    for (let i = 1; i <= count; i++){
+        resultString += text;
+    }
+    return resultString;
 }
 
-//4.Ticket Pricing:
+console.log(repeatString('a,b,c', 3));
 
-let age = 19;
+//10.Simple Calculator:
 
-if(age < 5){
-    console.log('Free');
-} else if (age >= 5 && age <= 12){
-    console.log("Child Ticket: $5");
-} else if (age >= 13 && age <= 59){
-    console.log("Adult Ticket: $10");
-} else {
-    console.log("Senior Ticket: $7");
+function calculate(num1, num2, operation){
+    if(operation === '+'){
+        return num1 + num2;
+    } else if (operation === '-'){
+        return num1 - num2;
+    } else if (operation === '*' && num1 !== 0 && num2 !== 0){
+        return num1 * num2;
+    } else if (operation === '/' && num1 !== 0 && num2 !== 0){
+        return num1 / num2;
+    } else {
+        return 'null';
+    }
 }
 
-//5.Switch with Fall-through (Days of the Week):
+console.log(calculate(3, 0, '*'));
 
-let dayNumber = '4';
+//Hard:
 
-switch(dayNumber){
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-        console.log('Weekday');
-        break;
-    case '6':
-    case '7':
-        console.log('Weekend');
-        break;
-    default:'Invalid numbers'
+//11. Number Range Checker:
+
+function isWithinRange(num, lowerBound, upperBound){
+    if(num >= lowerBound && num <= upperBound){
+        return true;
+    } else {
+        return false;
+    }
 }
+console.log(isWithinRange(15, 10, 20));
 
-//Hard Exercises
+//12.Factorial Calculator:
 
-//1.Nested Access Control:
-
-let isLoggedIn = true;
-let userRole = 'admin';
-let documentStatus = 'draft';
-
-if(!isLoggedIn){
-    console.log('Please log in.');
-} else {
-    if(userRole === 'admin'){
-        console.log('Admin can manage all documents.');
-    } else if (userRole === 'editor'){
-        if(documentStatus === 'draft'){
-            console.log('Editor can edit drafts.');
-        } else if (documentStatus === 'published'){
-            console.log('Editor cannot edit published documents.');
-        } else {
-            console.log('Invalid status');
+function calculateFactorial(num){
+    if(num === 0){
+        return 1;
+    } else if (num < 0){
+        return null;
+    } else {
+        let numFact = 1;
+        let i = 1;
+        while(i <= num){
+            numFact = numFact * i;
+            i++
         }
-    } else if(userRole === 'guest'){
-        if(documentStatus === 'published'){
-            console.log('Guest can view published documents.')
-        } else if (documentStatus === 'draft'){
-            console.log('Guest cannot view drafts.')
+        return numFact;
+    }
+}
+
+console.log(calculateFactorial(5));
+
+//13. Simple Password Validation:
+
+function isPasswordValid(password){
+    if(password.length >= 8){
+        if(password === 'paroliaigi'){
+            return true;
         } else {
-            console.log('Invalid status');
-        } 
-    } else {
-        console.log('Uncnown role.')
+            return false;
+        }
     }
 }
-
-//2.Simple Discount Calculator:
-
-let purchaseAmount = 10;
-let isMember = true;
-
-if(purchaseAmount >= 100){
-    if(isMember === true){
-        console.log(`Amount: ${purchaseAmount}, discount 15%, Final price: ${purchaseAmount - (purchaseAmount * 15 / 100)}`);
-    } else {
-        console.log(`Amount: ${purchaseAmount}, discount 10%, Final price: ${purchaseAmount - (purchaseAmount * 10 / 100)}`);
-    }
-} else if(purchaseAmount < 100){
-    if(isMember === true){
-        console.log(`Amount: ${purchaseAmount}, discount 5%, Final price: ${purchaseAmount - (purchaseAmount * 5 / 100)}`);
-    } else {
-        console.log(`Amount: ${purchaseAmount}, discount 0%, Final price: ${purchaseAmount}`);
-    }
-}else {
-    console.log(purchaseAmount);
-}
-
-//3.Rock Paper Scissors Logic:
-
-let player1Choice = 'rock';
-let player2Choice = 'paper';
-
-if (player1Choice === 'rock'){
-    if (player2Choice === 'paper'){
-        console.log('Player 2 wins!');
-    } else if (player2Choice === 'scissors'){
-        console.log('Player 1 wins!');
-    } else {
-        console.log("it's a tie!");
-    }
-} else if (player1Choice === 'paper'){
-    if (player2Choice === 'paper'){
-        console.log("it's a tie!");
-    } else if (player2Choice === 'scissors'){
-        console.log('Player 2 wins!');
-    } else {
-        console.log('Player 1 wins!');
-    }
-} else if (player1Choice === 'scissors'){
-    if (player2Choice === 'paper'){
-        console.log('Player 1 wins!');
-    } else if (player2Choice === 'scissors'){
-        console.log("it's a tie!");
-    } else {
-        console.log('Player 2 wins!');
-    }
-} else {
-    console.log('invalid move');
-}
-
-//4.Grade Calculator using Switch (Truthy expression):
-
-let score = 90;
-
-switch(true){
-    case score >= 90:
-        console.log("Grade: A");
-        break;
-    case score >= 80:
-        console.log("Grade: B");
-        break;
-    case score >= 70:
-        console.log("Grade: C");
-        break;
-    case score >= 60:
-        console.log("Grade: D");
-        break;
-    default: 
-        console.log("grade: F")
-}
-
-//5.Character Class Assignment:
-
-let strength = 40;
-let intelligence = 45;
-let className = '';
-
-if(strength > 15 && intelligence <= 10){
-    className = 'Warrior';
-} else if (strength <= 10 && intelligence > 15){
-    className = 'Mage';
-} else if (strength > 10 && intelligence > 10){
-    className = 'Spellsword';
-} else {
-    className = 'Advanturer'
-}
-
-console.log(className);
