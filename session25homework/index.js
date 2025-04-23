d43454c9dcb6b66e8e66fa42cf13d00644334bc7
webGrandpa <goga.khilo@gmail.com>
2025-04-23 08:09:32 +0400

    homework

--- session25homework/index.js
+++ session25homework/index.js
@@ -7,8 +7,103 @@ function countDownFrom(n) {
 countDownFrom(7);
 
 //Task 2: Iterate Over an Array (For...of Loop)
-function says(){
-    return"he sayd 'its cool'";
+
+function printArrayElements(arr){
+    for (let fruit of arr) {
+        console.log(fruit)
+    }
 }
 
-console.log(says());
+printArrayElements(["Apple", "Banana", "Cherry"]);
+
+// task 3: Sum of Numbers (While Loop)
+
+function sumNumbersUpTo(n){
+    let calculate = 0;
+    while(n > 0){
+        calculate += n;
+        n--
+    }
+    return calculate
+}
+
+console.log(sumNumbersUpTo(5));
+
+//Task 4: Repeat a String (Do...While Loop)
+
+//4.1
+
+function repeatString(str, n) {
+    let result = "";
+    let count = 0;
+    do{
+        result = result + str;
+        count = count + 1;
+    } while (count < n);
+    console.log(result);
+}
+
+console.log(`Santa-clous says:, ${repeatString("how", 3)}`);
+
+//4,2
+
+function climbTower(levels) {
+    let climb = 0;
+    do{
+        climb++;
+    } while (climb < levels);
+    console.log(`You climbed to level: ${climb}`);
+}
+console.log(climbTower(8));
+
+//4.3
+
+function countDownSpell(n){
+    do{
+        console.log(n);
+        n--;
+    } while (n > 0);
+}
+console.log( countDownSpell(8));
+
+//4.4
+
+function chantMantra(mantra, times) {
+    let count = 1;
+    do{
+        console.log(`${count}: ${mantra}`);
+        count++;
+    } while(count <= times);
+}
+console.log(chantMantra("OMG", 6));
+
+//4.5
+
+function chargeEnergy(current, max){
+    do{
+        console.log(`Energy level: ${current}`);
+        current += 10;
+    } while(max >= current);
+}
+console.log(chargeEnergy(10, 100));
+//უკედ რომ გამეგო ამდაგვარი დავალებები შევასრულე
+
+//Task 5: Iterate Over Object Properties (For...in Loop):
+
+function printObjectKeys(obj){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
