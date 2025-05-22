b303c95dfd8d629bf356828ecd4ce9e4f155d6de
webGrandpa <goga.khilo@gmail.com>
2025-05-22 17:58:47 +0400

    Furniture website

--- Furniture_website/script/header.js
+++ Furniture_website/script/header.js
@@ -1,20 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerButton = document.querySelector('.hamburger-menu');
     const mobileMenuContainer = document.querySelector('.mobile-nav');
+    const mobileSearch = document.querySelector(".mobile-search");
 
-    if (hamburgerButton && mobileMenuContainer) {
+    if (hamburgerButton && mobileMenuContainer && mobileSearch) {
         hamburgerButton.addEventListener('click', () => {
             mobileMenuContainer.classList.toggle('active');
             hamburgerButton.classList.toggle('is-active');
+
+            if (mobileMenuContainer.classList.contains('active')) {
+                mobileSearch.style.display = 'none';
+            } else {
+                mobileSearch.style.display = 'flex';
+            }
         });
 
         mobileMenuContainer.addEventListener('click', (event) => {
             if (event.target === mobileMenuContainer) {
                 mobileMenuContainer.classList.remove('active');
                 hamburgerButton.classList.remove('is-active');
+
+                mobileSearch.style.display = 'flex';
             }
         });
     } else {
-        console.error('Error: Could not find hamburger menu button or mobile menu container.');
+        console.error('Error: Could not find one or more required elements (hamburger menu, mobile menu container, or mobile search).');
     }
 });
